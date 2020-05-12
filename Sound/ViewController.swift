77894f0fc9c2740f166cade8db4669588992eb68
yukiees <yukiii8989@gmail.com>
2020-05-12 16:51:23 +0900

    sound app complete

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -13,10 +13,16 @@ class ViewController: UIViewController {
     
     @IBOutlet var drumButton: UIButton!
     
+    @IBOutlet var pianoButton: UIButton!
+    
+    @IBOutlet var guitarButton: UIButton!
+    
     
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
     
+    let pianoSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "pianoSound")!.data)
     
+     let guitarSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "guitarSound")!.data)
     
     
 
@@ -38,5 +44,34 @@ class ViewController: UIViewController {
     @IBAction func touchUpDrumbutton(){
         drumButton.setImage(UIImage(named: "drumImage"),for: .normal)
     }
+    
+    @IBAction func touchDownPianobutton(){
+        
+        pianoButton.setImage(UIImage(named: "pianoPlayingImage"),for: .normal)
+        
+        
+        pianoSoundPlayer.currentTime = 0
+        
+        pianoSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpPianobutton(){
+           pianoButton.setImage(UIImage(named: "pianoImage"),for: .normal)
+       }
+    
+    
+       @IBAction func touchDownGuitarbutton(){
+           
+           guitarButton.setImage(UIImage(named: "guitarPlayingImage"),for: .normal)
+           
+           
+           guitarSoundPlayer.currentTime = 0
+           
+           guitarSoundPlayer.play()
+       }
+       
+       @IBAction func touchUpGuitarbutton(){
+              guitarButton.setImage(UIImage(named: "guitarImage"),for: .normal)
+          }
 }
 
